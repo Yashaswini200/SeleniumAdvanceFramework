e95c85e85a46caf366f881abcf0da97b6b04e851
Yashaswini200 <yashusuresh2050@gmail.com>
2025-06-05 20:50:39 +0530

    commit

---
+++ src/test/java/org/example/base/Base_test.java
@@ -0,0 +1,16 @@
+package org.example.base;
+
+import org.example.driver.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base_test { @BeforeMethod
+public void setUp(){
+    DriverManager.init();
+}
+
+    @AfterMethod
+    public void tearDown(){
+        DriverManager.down();
+    }
+}
