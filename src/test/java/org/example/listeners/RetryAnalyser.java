b457a7763b00f134fe140968361ca4cbc5182275
Yashaswini200 <yashusuresh2050@gmail.com>
2025-06-22 13:38:45 +0530

    commit

--- src/test/java/org/example/listeners/RetryAnalyser.java
+++ src/test/java/org/example/listeners/RetryAnalyser.java
@@ -1,9 +1,19 @@
 package org.example.listeners;
 
+import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 import org.testng.util.RetryAnalyzerCount;
 
-public class RetryAnalyser {
+public class RetryAnalyser implements IRetryAnalyzer {
+    private  int retrycounter=0;
+    private static final int maxRetryCount=3;
+    @Override
+    public boolean retry(ITestResult iTestResult){
+        if(retrycounter<maxRetryCount){
+            retrycounter++;
+            return true;
+        }return false;
+    }
 
 
 }
